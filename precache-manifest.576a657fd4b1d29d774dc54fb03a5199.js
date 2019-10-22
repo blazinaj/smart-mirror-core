e2c2ffb82860f33a8a3e1200d091c902b856e224
Jacob Blazina <blazinaj@hotmail.com>
2019-10-21 19:43:59 -0700

    Updates

---
+++ precache-manifest.576a657fd4b1d29d774dc54fb03a5199.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b206a6a668c5ffa7bab3acf92e7521fb",
+    "url": "/smart-mirror-core/index.html"
+  },
+  {
+    "revision": "b84f34730db4edaa9d44",
+    "url": "/smart-mirror-core/static/css/2.1ad9c816.chunk.css"
+  },
+  {
+    "revision": "9ece731bee2c75d9bf82",
+    "url": "/smart-mirror-core/static/css/main.abd8a3fa.chunk.css"
+  },
+  {
+    "revision": "b84f34730db4edaa9d44",
+    "url": "/smart-mirror-core/static/js/2.377e6e45.chunk.js"
+  },
+  {
+    "revision": "9ece731bee2c75d9bf82",
+    "url": "/smart-mirror-core/static/js/main.50498f3b.chunk.js"
+  },
+  {
+    "revision": "945e3b9d93bca44a83ec",
+    "url": "/smart-mirror-core/static/js/runtime-main.038e1885.js"
+  }
+]);
