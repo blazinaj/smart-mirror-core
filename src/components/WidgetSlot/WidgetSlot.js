3828cc378395815430a9473ff2109b01c975997d
Jacob Blazina <jacob@rivialsecurity.com>
2019-10-28 09:24:08 -0700

    WIP playing around with widget slots

--- src/components/WidgetSlot/WidgetSlot.js
+++ src/components/WidgetSlot/WidgetSlot.js
@@ -1,24 +1,66 @@
+/** WIP - DO NOT USE
+ * Author: Jacob Blazina
+ *
+ * Description: Playing around with generic, moveable, and resizeable widget slots
+ */
+
 import React from "react";
 import PropTypes from "prop-types";
+import Draggable from 'react-draggable';
+import {Card, CardBody, CardHeader} from "reactstrap";
+import { Resizable, ResizableBox } from 'react-resizable';
+import './css/styles.css';
+import './css/test.css';
 
 const WidgetSlot = (props) => {
 
     return (
-        <div
-            style={{
-                border: "dashed",
-            }}
+        <Draggable
+            bounds=".bounds"
+            axis="both"
+            handle=".handle"
+            defaultPosition={{x: 50, y: 50}}
+            position={null}
+            grid={[25, 25]}
+            scale={1}
         >
+            <ResizableBox
+                style={{backgroundColor: "white"}}
+                width={20}
+                height={20}
+                //draggableOpts={{...}}
+               // minConstraints={[100, 100]}
+               // maxConstraints={[300, 300]}
+            >
+
             {
-                props.widget || props.children
+                props.enableEdits ?
+                    <Card>
+                        <CardHeader className="handle">
+                            Edit Widget
+                        </CardHeader>
+                        <CardBody>
+                            {
+                                props.widget || props.children
+                            }
+                        </CardBody>
+                    </Card>
+                    :
+                    props.widget || props.children
             }
-        </div>
+            </ResizableBox>
+        </Draggable>
     )
 
-}
+};
 
 WidgetSlot.propTypes = {
-    widget: PropTypes.func.isRequired
-}
+    widget: PropTypes.func,
+    enableEdits: PropTypes.bool
+};
+
+WidgetSlot.defaultProps = {
+    enableEdits: false
+};
 
 export default WidgetSlot;
