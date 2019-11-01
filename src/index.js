91063bafc959f89dbbbcf8d2de48d4819f791e31
Anatoli Railean <arailean@eagles.ewu.edu>
2019-10-31 17:32:50 -0700

    Added route switch

--- src/index.js
+++ src/index.js
@@ -5,9 +5,22 @@ import 'react-grid-layout/css/styles.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-resizable/css/styles.css';
 import App from './App';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
 import * as serviceWorker from './serviceWorker';
+import Configuration from "./views/Config/Config";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const routing = (
+    <Router>
+        <div>
+            <Switch>
+                <Route exact path="/" component={App} />
+                <Route path="/config" component={Configuration} />
+            </Switch>
+        </div>
+    </Router>
+);
+
+ReactDOM.render(routing, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
