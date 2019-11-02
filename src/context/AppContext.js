8d8f4e58284ee117917d249e563977dce9f646be
Jacob Blazina <blazinaj@hotmail.com>
2019-11-02 16:34:49 -0700

    Created Default Layout components and Application Context

---
+++ src/context/AppContext.js
@@ -0,0 +1,7 @@
+/**
+ * Description: Definition for the AppContext object that will be used to hold Mongo DB hooks.
+ */
+
+import React from "react";
+
+export const AppContext = React.createContext({});
