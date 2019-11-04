15d3e0441bcc6a659ea04b7f5d4571d121358a3e
Nikolas Graham <Lichtschwert@live.com>
2019-11-04 13:22:22 -0800

    WIP - First Design - Login Screen

--- src/views/Login/Login.js
+++ src/views/Login/Login.js
@@ -5,27 +5,77 @@
  *              NOTE: AutoConfirm is currently turned on in Stitch settings.
  */
 
-import {useState} from "react";
+import {useState, useContext} from "react";
 import PropTypes from "prop-types";
-import {Button, Card, CardBody, CardHeader, Input} from "reactstrap";
+import {
+    Alert, Button, Collapse, DropdownItem, DropdownMenu, DropdownToggle,
+    Form, Input, InputGroup, InputGroupAddon, Nav, Navbar, NavbarBrand,
+    NavbarToggler, UncontrolledDropdown
+} from "reactstrap";
 import React from "react";
+import Styles from "./css/styles.css";
 
 const Login = (props) => {
-    const [email, setEmail] = useState('blazinaj@hotmail.com');
-    const [password, setPassword] = useState('CSCD488');
+    //These setting store state of email and password
+    const [email, setEmail] = useState('lichtschwert@live.com');
+    const [password, setPassword] = useState('FakePassword');
+
+    //Login function
+    const login = () => {
+        props.mongoHook.login(email, password);
+        /*if(props.mongoHook.isLoggedIn === false){
+            setVisibleIncorrectInformation();
+        }*/
+    };
+
+    //These setting hide and display developer dropdown menu
+    const [isOpenNav, setIsOpenNav] = useState(false);
+    const toggleNav = () => setIsOpenNav(!isOpenNav);
+
+    //These setting hide and display the alert
+    const [visibleIncorrectInformation, setVisibleIncorrectInformation] = useState(false);
+    const onDismiss = () => setVisibleIncorrectInformation(false);
 
     return (
-        <Card>
-            <CardHeader>
-                Login
-            </CardHeader>
-            <CardBody>
-                <Input value={email} placeholder="Email" onChange={(e) => setEmail(e.target.value)}/>
-                <Input value={password} placeholder="Password" onChange={(e) => setPassword(e.target.value)}/>
-                <Button onClick={() => props.mongoHook.login(email, password)}>Login</Button>
-                <Button onClick={() => props.mongoHook.register(email, password)}>Register</Button>
-            </CardBody>
-        </Card>
+        <div id="loginPage">
+            <Navbar style={Styles} id="navbar" light expand="md">
+                <img src={require('./images/logo-low_quality.png')} />
+                <NavbarBrand id="companyName" stlye={Styles} href="/">OpTech</NavbarBrand>
+                <NavbarToggler onClick={toggleNav} />
+                <Collapse isOpen={isOpenNav} navbar>
+                    <Nav className="ml-auto" navbar>
+                        <UncontrolledDropdown nav inNavbar>
+                            <DropdownToggle id="dropdown" nav caret>
+                                Developers
+                            </DropdownToggle>
+                            <DropdownMenu right id="dropdownMenu">
+                                <DropdownItem className="dropdownItem" href="https://github.com/blazinaj/smart-mirror-core/tree/master">
+                                    Github
+                                </DropdownItem>
+                            </DropdownMenu>
+                        </UncontrolledDropdown>
+                    </Nav>
+                </Collapse>
+            </Navbar>
+            <Alert color="info" isOpen={visibleIncorrectInformation} toggle={onDismiss}>
+                Incorrect email or password!
+            </Alert>
+            <Form>
+                <div id={"inputFieldsLogin"}>
+                    <InputGroup className={"inputGroupLogin"}>
+                        <InputGroupAddon className={"pre-pend"} addonType="prepend">Email</InputGroupAddon>
+                        <Input className={"inputField"} type="email" value={email} placeholder="Email..." onChange={(e) => setEmail(e.target.value)}/>
+                    </InputGroup>
+                    <br />
+                    <InputGroup className={"inputGroupLogin"}>
+                        <InputGroupAddon className={"pre-pend"} addonType="prepend">Password</InputGroupAddon>
+                        <Input className={"inputField"} type="password" value={password} placeholder="Password..." onChange={(e) => setPassword(e.target.value)}/>
+                    </InputGroup>
+                </div>
+                <Button className={"authButton"}  onClick={() => login()}>Login</Button>
+                <Button className={"authButton"}  onClick={() => props.mongoHook.register(email, password)}>Register</Button>
+            </Form>
+        </div>
     )
 };
 
