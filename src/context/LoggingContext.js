c98807e00b55a1dfca9c6054e1df99d83e97cadb
Jacob Blazina <blazinaj@hotmail.com>
2019-12-03 22:40:48 -0800

    Created a logger context

---
+++ src/context/LoggingContext.js
@@ -0,0 +1,7 @@
+/**
+ * Description: Definition for the LoggingContext object that will be used to hold Logging hooks.
+ */
+
+import React from "react";
+
+export const LoggingContext = React.createContext({});
