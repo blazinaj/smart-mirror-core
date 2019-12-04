be286bf9b9a397afc60a3ef1fe1de027339ca850
Jacob Blazina <blazinaj@hotmail.com>
2019-12-03 22:41:00 -0800

    Created a useDatabase hook

---
+++ src/hooks/useDatabase.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from "react";
+import {Stitch, RemoteMongoClient} from "mongodb-stitch-browser-sdk";
+
+export const useDatabase = () => {
+
+    let client = Stitch.defaultAppClient;
+    const db = client.getServiceClient(RemoteMongoClient.factory, 'mongodb-atlas').db('smart_mirror');
+
+    const find = (collection, query) => {
+        return db.collection(collection).find(query)
+    };
+
+    const findOne = (collection, query) => {
+        return db.collection(collection).findOne(query)
+    };
+
+    const findMany = (collection, query) => {
+        /*
+                collection: "face_descriptors"
+                to get all documents in collection: find({})
+         */
+        return db.collection(collection).find(query).asArray()
+    };
+
+    const insertOne = (collection, mutation) => {
+        return db.collection(collection).updateOne(mutation)
+    };
+
+    const insertMany = (collection, mutation) => {
+        return db.collection(collection).updateOne(mutation)
+    };
+
+    const updateOne = (collection, mutation) => {
+        return db.collection(collection).updateOne(mutation)
+    };
+
+    return {
+        user: client.auth.user,
+        find,
+        findMany,
+        findOne,
+        insertOne,
+        insertMany,
+        updateOne,
+    }
+
+};
