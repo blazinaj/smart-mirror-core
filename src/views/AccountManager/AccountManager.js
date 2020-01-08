4b38657695ccc8546c9636b211eadacf121f0d77
Nikolas Graham <Lichtschwert@live.com>
2020-01-08 15:49:22 -0800

    Account manager can change email/name

--- src/views/AccountManager/AccountManager.js
+++ src/views/AccountManager/AccountManager.js
@@ -1,28 +1,52 @@
-import React, {useContext, useState} from "react";
-import {Button, Collapse, Input, InputGroup, InputGroupAddon} from "reactstrap"
+import React, {useContext, useState, useEffect} from "react";
+import {Button, Collapse, Input, InputGroup, InputGroupAddon,
+        Alert} from "reactstrap"
 import {AppContext} from "../../context/AppContext";
-import {useProfile} from "../../hooks/useProfile";
 import GoogleCalendarConfig from "../Google/GoogleCalendarConfig";
 import FaceLoginSetup from "../Config/FaceLoginSetup/FaceLoginSetup";
+import {useDatabase} from "../../hooks/useDatabase";
 
 const AccountManager = (props) => {
 
-    // state for user info?
-    // const [isFaceSetupOpen, setFaceSetupOpen] = useState(false);
-    // const toggleFaceSetup = () => setFaceSetupOpen(!isFaceSetupOpen);
+    // First Name State
+    const [firstName, setFirstName] = useState("EMPTY");
+    const [changeFirstName, setChangeFirstName] = useState("");
 
-    // <Button color="info" onClick={toggleFaceSetup} style={{ marginBottom: '1rem' }}>User Info</Button>
-    // <Collapse isOpen={isFaceSetupOpen}>
-    //    <label>User Info</label>
-    // </Collapse>
+    // Last Name State
+    const [lastName, setLastName] = useState("EMPTY");
+    const [changeLastName, setChangeLastName] = useState("");
 
-    //-------------------------------------------------------------
+    // Email State
+    const [email, setEmail] = useState("EMPTY");
+    const [changeEmail, setChangeEmail] = useState("");
 
-    const [firstName, setFirstName] = useState("Firstname");
-    const [lastName, setLastName] = useState("Lastname");
+    //TEMP - PASSWORD STATE
+    const [password, setPassword] = useState(""); //DEV USE ONLY - DELETE AFTER TESTING
 
+    // Reset Password States
+    const [resetPasswordOne, setResetPasswordOne] = useState("");
+    const [resetPasswordTwo, setResetPasswordTwo] = useState("");
+    const [passwordsMatch, setPasswordsMatch] = useState("Enter your new password...");
+
+    // Redraw Page State
+    const [updateInfo, setUpdateInfo] = useState(false);
+
+    // Collapsing Drawer States
     const [isInfoSetupOpen, setInfoSetupOpen] = useState(false);
-    const toggleInfoSetup = () => setInfoSetupOpen(!isInfoSetupOpen);
+    const toggleInfoSetup = () => {
+        if(isResetPasswordOpen){
+            toggleResetPassword();
+        }
+        setInfoSetupOpen(!isInfoSetupOpen);
+    };
+
+    const [isResetPasswordOpen, setResetPasswordOpen] = useState(false);
+    const toggleResetPassword = () => {
+        if(isInfoSetupOpen){
+            toggleInfoSetup()
+        }
+        setResetPasswordOpen(!isResetPasswordOpen);
+    };
 
     const [isFaceSetupOpen, setFaceSetupOpen] = useState(false);
     const toggleFaceSetup = () => setFaceSetupOpen(!isFaceSetupOpen);
@@ -31,34 +55,124 @@ const AccountManager = (props) => {
     const toggleGoogle = () => setIsGoogleOpen(!isGoogleOpen);
 
     const context = useContext(AppContext);
-    const profileHook = useProfile();
+    const userInfo = useDatabase();
 
-    //Notes for later when DB is working again in local
+    useEffect(() => {
+        const getInfo = async () => {
+            let info = await userInfo.findOne('face_descriptors', {owner_id: context.mongoHook.authenticatedUser.id})
+                .then(authedUserInfo => {
+                    return authedUserInfo;
+                });
+            if (info != null) {
+                setPassword(info.password); //DEV USE ONLY - DELETE AFTER TESTING
+            }
+        };
+
+        const getUserInfo = async () => {
+            let info = await userInfo.findOne('users', {userId: context.mongoHook.authenticatedUser.id})
+                .then(authedUserInfo => {
+                    return authedUserInfo;
+                });
+            if (info != null) {
+                setEmail(info.email);
+                setFirstName(info.first_name);
+                setLastName(info.last_name);
+            }
+        };
+
+        getInfo();
+        getUserInfo();
+
+        if(resetPasswordOne === "" || resetPasswordTwo === ""){
+            setPasswordsMatch("Enter your new password...");
+        }
+        else if(resetPasswordOne === resetPasswordTwo){
+            setPasswordsMatch("Passwords Match!");
+        }
+        else {setPasswordsMatch("Passwords do not match!")}
+
+    }, [updateInfo, resetPasswordOne, resetPasswordTwo]);
+
+    const changeInfo = async () => {
+        let msg = await userInfo.updateOne("users", {userId: context.mongoHook.authenticatedUser.id},
+            {$set: {email: changeEmail, first_name: changeFirstName, last_name: changeLastName}});
+        setUpdateInfo(!updateInfo);
+        switch(msg.matchedCount){
+            case 0: msg = "Matched: 0\nAccount not found!";
+                        break;
+            case 1: msg = "Matched: 1\nAccount found!\n"+context.mongoHook.authenticatedUser.id;
+        }
+        console.log(msg);
+    };
+
+    const deleteAccount = () => {
+        setVisibleDeleteConfirmation(!visibleDeleteConfirmation);
+    };
+
+    const deleteAccountConfirmation = () => {
+      alert("Account Deleted! I guess....\nBye Bye =,(")
+    };
+
+    const [visibleDeleteConfirmation, setVisibleDeleteConfirmation] = useState(false);
+    const onDismissDeleteConfirmation = () => setVisibleDeleteConfirmation(false);
+
+    //Notes
     /*
-        change the way firstname/lastname input interacts with the page so it doesn't automatically change the value,
-        and instead changes the database on confirm and the label pulls from database
+        User pool pass and descriptor pass are separate from eachother.
+        Modification to both use pool pass and database plain text are required until changed!
 
-
+        For the delete feature it will need to delete the users/descriptors value of itself, and the user
+        from the user pool
      */
 
     return (
         <div>
 
-            <label>{firstName} {lastName}</label>
+            <label>UserID: {context.mongoHook.authenticatedUser.id}</label>
+            <br />
+            <label>Name: {firstName} {lastName}</label>
+            <br />
+            <label>Email: {email}</label>
+            <br />
+            <label>(DEV USE ONLY) Password: {password}</label>
 
             <br />
             <br />
 
             <Button color="primary" onClick={toggleInfoSetup} style={{ marginBottom: '1rem' }}>Edit Personal Info</Button>
+            <label>--</label>
+            <Button color="primary" onClick={toggleResetPassword} style={{ marginBottom: '1rem' }}>Change Password</Button>
             <Collapse isOpen={isInfoSetupOpen}>
-                <label>WIP</label>
+                <label>WIP - New Info</label>
                 <InputGroup className={"inputGroupLogin"}>
                     <InputGroupAddon className={"pre-pend"} addonType="prepend">FirstName</InputGroupAddon>
-                    <Input className={"inputField"} placeholder="Firstname..." onChange={(e) => setFirstName(e.target.value)}/>
+                    <Input className={"inputField"} placeholder="Firstname..." onChange={(e) => setChangeFirstName(e.target.value)}/>
                 </InputGroup>
                 <InputGroup className={"inputGroupLogin"}>
                     <InputGroupAddon className={"pre-pend"} addonType="prepend">LastName</InputGroupAddon>
-                    <Input className={"inputField"} placeholder="Lastname..." onChange={(e) => setLastName(e.target.value)}/>
+                    <Input className={"inputField"} placeholder="Lastname..." onChange={(e) => setChangeLastName(e.target.value)}/>
+                </InputGroup>
+                <br />
+                <InputGroup className={"inputGroupLogin"}>
+                    <InputGroupAddon className={"pre-pend"} addonType="prepend">Email</InputGroupAddon>
+                    <Input className={"inputField"} type="email" placeholder="Email..." onChange={(e) => setChangeEmail(e.target.value)}/>
+                </InputGroup>
+                <br />
+                <Button color="danger" onClick={changeInfo}>Confirm</Button>
+            </Collapse>
+
+            <Collapse isOpen={isResetPasswordOpen}>
+                <label>{resetPasswordOne} :::WIP::: {resetPasswordTwo}</label>
+                <br />
+                <label>{passwordsMatch}</label>
+                <InputGroup className={"inputGroupLogin"}>
+                    <InputGroupAddon className={"pre-pend"} addonType="prepend">New Password</InputGroupAddon>
+                    <Input className={"inputField"} type="password" placeholder="..." onChange={(e) => setResetPasswordOne(e.target.value)}/>
+                </InputGroup>
+                <br />
+                <InputGroup className={"inputGroupLogin"}>
+                    <InputGroupAddon className={"pre-pend"} addonType="prepend">New Password</InputGroupAddon>
+                    <Input className={"inputField"} type="password" placeholder="..." onChange={(e) => setResetPasswordTwo(e.target.value)}/>
                 </InputGroup>
                 <br />
                 <Button color="danger">Confirm</Button>
@@ -68,7 +182,7 @@ const AccountManager = (props) => {
 
             <Button color="info" onClick={toggleFaceSetup} style={{ marginBottom: '1rem' }}>Face Login Setup</Button>
             <Collapse isOpen={isFaceSetupOpen}>
-                <label>403: Stitch Client Unavailable</label>
+                <FaceLoginSetup/>
             </Collapse>
 
             <hr />
@@ -77,6 +191,15 @@ const AccountManager = (props) => {
             <Collapse isOpen={isGoogleOpen}>
                 <GoogleCalendarConfig/>
             </Collapse>
+
+            <hr />
+
+            <Button color="danger" onClick={deleteAccount}>Delete Account</Button>
+            <Alert color="danger" isOpen={visibleDeleteConfirmation} toggle={onDismissDeleteConfirmation}>
+                <label>Are you sure? </label>
+                &nbsp;
+                <Button color="danger" onClick={deleteAccountConfirmation}>Confirm</Button>
+            </Alert>
         </div>
     )
 
