0c9e10ab442cfbd6b18520ef782bc4032043fceb
Jacob Blazina <blazinaj@hotmail.com>
2020-01-13 19:58:29 -0800

    Stubbed out face demo page component

---
+++ src/containers/FaceDemo/FaceDemo.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+
+const FaceDemo = (props) => {
+
+    return (
+        <div style={{height: "100vh", background: "black"}}>
+            <h1>Face Demo Page</h1>
+        </div>
+    )
+
+};
+
+export default FaceDemo;
