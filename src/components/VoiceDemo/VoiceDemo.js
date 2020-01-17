6edebec291b9a217a9609b51beb4a579a920396f
Unknown <jhowes@eagles.ewu.edu>
2020-01-16 19:18:07 -0800

    Added joke text, show joke text toggle, added initial grid

--- src/components/VoiceDemo/VoiceDemo.js
+++ src/components/VoiceDemo/VoiceDemo.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect, useContext} from "react";
 import {VoiceCommandsContext} from "../../context/VoiceCommandsContext";
-import {Input, Label, Button} from 'reactstrap';
+import {Input, Label, Button, Row, Col} from 'reactstrap';
 import useSpeechSynthesis from "../../hooks/useSpeechSynthesis";
 
 
@@ -8,10 +8,20 @@ const VoiceDemo = (props) => {
 
     const {SpeechRecognitionHook} = useContext(VoiceCommandsContext);
     const speechSynthesisHook = useSpeechSynthesis();
+    const [showJoke, setShowJoke] = useState(true);
+    const [joke, setJoke] = useState("");
 
     const whoIsTheManCommand = {
         command: ["mirror mirror on the wall who is the man", "mirror mirror who is the man"],
-        answer: "Stu is the man"
+        answer: "Jeff is the man"
+    };
+
+    const showJokeText = {
+        command: ["mirror mirror toggle joke text"],
+        answer: "toggled",
+        func: () => {
+            showJoke ? setShowJoke(false) : setShowJoke(true);
+        }
     };
 
     const tellJoke = {
@@ -28,15 +38,18 @@ const VoiceDemo = (props) => {
                 return response.json();
             })
             .then((myJson) => {
+
                 console.log(myJson);
                 // alert(myJson.joke);
                 if (myJson.type === "single") {
                     // alert("Single " + myJson.joke)
                     speechSynthesisHook.speak(myJson.joke)
+                    setJoke(myJson.joke);
                 } else {
                     //alert(myJson.setup + " " +  myJson.delivery)
                     const twoPartJoke = myJson.setup + myJson.delivery;
                     speechSynthesisHook.speak(twoPartJoke)
+                    setJoke(twoPartJoke);
                 }
             });
     }
@@ -44,19 +57,57 @@ const VoiceDemo = (props) => {
     useEffect(() => {
         SpeechRecognitionHook.addCommand(whoIsTheManCommand);
         SpeechRecognitionHook.addCommand(tellJoke);
+        SpeechRecognitionHook.addCommand(showJokeText);
     }, []);
 
 
     return (
 
-        <div style={{background: "black", height: "100vh"}}>
+        <div style={{height: "100vh", background: "black", padding: "5vw"}}>
             <h1>Voice Demo Page</h1>
             <>
                 {
                     SpeechRecognitionHook.displayTranscript
                 }
             </>
-            <Button onClick={() => getJoke()}>Search</Button>
+            <Col>
+                <Row>
+                    <Col lg={3}>
+                        <Row style={{height: "50vh"}}>
+                            <h4 style={{color: "white"}}>
+                                Example Voice Commands:
+                            </h4>
+                            <h6 style={{color: "white"}}>"Mirror Mirror tell me a joke."</h6>
+                            <h6 style={{color: "white"}}>"Mirror Mirror who is the man?"</h6>
+                            <h6 style={{color: "white"}}>"Mirror Mirror toggle joke text."</h6>
+                        </Row>
+                        <Row style={{height: "50vh"}}>
+                            <h4 style={{color: "white"}}>
+                                Other info:
+                            </h4>
+                        </Row>
+                    </Col>
+                    <Col lg={6}>
+                        <div style={{height: "50vh", margin: "auto auto", background: "black"}}>
+                            <h2 style={{color: "white"}}>Joke</h2>
+                            <h3 style={{color: "white"}}>{showJoke && joke}</h3>
+                        </div>
+                    </Col>
+                    <Col lg={3}>
+                        <Row style={{height: "50vh"}}>
+                            <h4 style={{color: "white"}}>
+                                Voice command and answer:
+                            </h4>
+                            <h5>{}</h5>
+                        </Row>
+                        <Row style={{height: "50vh"}}>
+                            <h4 style={{color: "white"}}>
+                                Fill stuff in:
+                            </h4>
+                        </Row>
+                    </Col>
+                </Row>
+            </Col>
         </div>
     );
 };
