19e7bd2dff063d9bb306987e9ded6b8d88cebc66
Jacob Blazina <blazinaj@hotmail.com>
2020-01-17 10:56:04 -0800

    Showing Age and Gender in FaceDemoPage

--- src/containers/FaceDemo/FaceDemo.js
+++ src/containers/FaceDemo/FaceDemo.js
@@ -1,7 +1,27 @@
 import React, { useState, useEffect } from "react";
-import {Col, Container, Row} from "reactstrap";
+import {Button, Col, Container, Row, Spinner} from "reactstrap";
+import useFace from "../../hooks/useFace";
 
 const FaceDemo = (props) => {
+
+    const faceApiHook = useFace();
+
+    const [ageAndGender, setAgeAndGender] = useState({});
+
+    let getAgeAndGender = async (withLoading) => {
+        let temp = await faceApiHook.getAgeAndGender(withLoading);
+        setAgeAndGender(temp);
+    };
+
+    useEffect(() => {
+
+        let initialLoad = true;
+        setInterval(async () => {
+            await getAgeAndGender(initialLoad);
+            initialLoad = false;
+        }, 1000)
+
+    }, []);
 
     return (
         <div style={{height: "100vh", background: "black", padding: "5vw"}}>
@@ -23,18 +43,51 @@ const FaceDemo = (props) => {
                     <Col lg={6}>
                         <div style={{height: "50vh", margin: "auto auto", background: "grey"}}>
                             <h4 style={{color: "white"}}>Camera Feed will go here?</h4>
+                            {faceApiHook.videoFeed}
                         </div>
                     </Col>
                     <Col lg={3}>
-                        <Row style={{height: "50vh"}}>
-                            <h4 style={{color: "white"}}>
-                                Gender Prediction:
-                            </h4>
+                        <Row style={{height: "50vh", color: "white"}}>
+                            <Col>
+                                <Row>
+                                    <h4 style={{color: "white"}}>
+                                        Gender Prediction:
+                                    </h4>
+                                </Row>
+                                <Row>
+                                    <h6>
+                                    {
+                                        faceApiHook.loadingAgeAndGender ?
+                                            <>
+                                                Loading Gender.. <Spinner color="primary"/>
+                                            </>
+                                            :
+                                        ageAndGender ? ageAndGender.gender : "No Gender Found"
+                                    }
+                                    </h6>
+                                </Row>
+                            </Col>
                         </Row>
-                        <Row style={{height: "50vh"}}>
-                            <h4 style={{color: "white"}}>
-                                Age Prediction:
-                            </h4>
+                        <Row style={{height: "50vh", color: "white"}}>
+                            <Col>
+                                <Row>
+                                    <h4 style={{color: "white"}}>
+                                        Age Prediction:
+                                    </h4>
+                                </Row>
+                                <Row>
+                                    <h6>
+                                    {
+                                        faceApiHook.loadingAgeAndGender ?
+                                            <>
+                                                Loading Age.. <Spinner color="primary"/>
+                                            </>
+                                            :
+                                            ageAndGender ? ageAndGender.age : "No Age Found"
+                                    }
+                                    </h6>
+                                </Row>
+                            </Col>
                         </Row>
                     </Col>
                 </Row>
