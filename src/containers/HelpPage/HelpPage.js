d97df3dc387cfe7b29833cc551592f7d931327cc
Nikolas Graham <Lichtschwert@live.com>
2020-01-18 21:44:05 -0800

    Cleaned up comments

--- src/containers/HelpPage/HelpPage.js
+++ src/containers/HelpPage/HelpPage.js
@@ -3,31 +3,13 @@ import {VoiceCommandsContext} from "../../context/VoiceCommandsContext";
 import {LoggingContext} from "../../context/LoggingContext";
 import {Col, Row} from "reactstrap";
 
-// WIP
-// Display most desireable info
-// Have command to show all commands (with certain amount per page and can say previous/next to change page)
 const HelpPage = () => {
 
     const voiceContext = useContext(VoiceCommandsContext).SpeechRecognitionHook;
     const loggingContext = useContext(LoggingContext).logger;
 
-    // const [pageAmount, setPageAmount] = useState(1);
-    // const [currentPage, setCurrentPage] = useState(0);
     const [commandList, setCommandList] = useState([[{}]]);
 
-    // useEffect(() => {
-    //     setPageAmount(voiceContext.intendArray.length / 15);
-    //     let newCommands = [];
-    //     for(let i=0; i < pageAmount; i++){
-    //         newCommands[i] = voiceContext.intendArray.slice(i, voiceContext.intendArray.length ? ((i+1) * 15)-1 : voiceContext.intendArray.length);
-    //     }
-    //     setCommandList(newCommands);
-    // }, [voiceContext.intendArray]);
-
-
-
-    //Test page not included
-    // Sleep/Logout/VoiceRegistration
     return (
         <div>
             <h1 style={{color: "teal"}}>Primary Voice Commands</h1>
@@ -47,17 +29,13 @@ const HelpPage = () => {
                     <h3 style={{color: "blue"}}>General</h3>
                     <label style={{color: "red", fontSize: "28px"}}>Show Speech to Text: </label><h5> Mirror mirror show transcript</h5>
                     <label style={{color: "red", fontSize: "28px"}}>Hide Speech to Text: </label><h5> Mirror mirror hide transcript</h5>
-                    {/*<h5>*/}
-                    {/*    It was our desire that our device could work entirely hands free using only voice and facial recognition.*/}
-                    {/*    Therefore we don't have any option to reset email/password currently as we have other goals to reach within our timeframe.*/}
-                    {/*</h5>*/}
+                    <label style={{color: "red", fontSize: "28px"}}>Create new Account: </label><h5> Mirror mirror register new account</h5>
                 </Col>
                 <Col>
                     <h3 style={{color: "blue"}}>Misc Commands</h3>
                     <h5><label style={{color: "red", fontSize: "28px"}}>Time: </label> Mirror mirror what time is it?</h5>
                 </Col>
                 <div>
-                    {/*<h4>Say: <h2 style={{color: "blue"}}>Mirror mirror close command list</h2> when finished.</h4>*/}
                     <h3 style={{color: "blue"}}>All Commands</h3>
                     <h5 style={{color: "blue"}}>(Not all commands function all the time)</h5>
                     {
@@ -69,8 +47,6 @@ const HelpPage = () => {
                                     })
                                 }
                                 <hr style={{backgroundColor: "red"}}/>
-                                {/*<li id={command}>{JSON.stringify(command.command[0])}</li>*/}
-                                {/*<br />*/}
                             </div>
                         })
                     }
