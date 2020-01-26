89da5564e6ba678b2fa6888c90a926df952a3ea9
Nikolas Graham <Lichtschwert@live.com>
2020-01-26 00:27:36 -0800

    Updated features, but no response anymore?

--- src/containers/Wikipedia/WikipediaSearchPage.js
+++ src/containers/Wikipedia/WikipediaSearchPage.js
@@ -1,5 +1,5 @@
 import React, {useState, useContext, useEffect} from "react"
-import {Button, Input, InputGroup, InputGroupAddon} from "reactstrap";
+import {Button, FormGroup, Input, InputGroup, InputGroupAddon, Label} from "reactstrap";
 import {LoggingContext} from "../../context/LoggingContext";
 import {VoiceCommandsContext} from "../../context/VoiceCommandsContext";
 
@@ -9,14 +9,16 @@ const WikipediaSearchPage = () => {
     const voiceContext = useContext(VoiceCommandsContext);
 
     const [searchQuery, setSearchQuery] = useState("");
-    const [foundResults, setFoundResults] = useState(["Hello", "Testing"]);
+    const [foundResults, setFoundResults] = useState([]);
     const [searchKeys, setSearchKeys] = useState([]);
 
-    const fetchResults = (resultQuantity) => {
-        //const endpoint = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=${searchQuery}`;
-          const endpoint = `https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=extracts|pageimages&pithumbsize=400&origin=*&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=${searchQuery}`;
-        fetch(endpoint)
-            .then(response => response.json())
+    // True for many results, false for one
+    const [searchQuantity, setSearchQuantity] = useState("many");
+
+    const fetchResults = async () => {
+        //const url = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=${searchQuery}`;
+        const url = `https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=extracts|pageimages&pithumbsize=400&origin=*&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=${searchQuery}`;
+        await fetch(url)
             .then(data => {
                 loggingContext.addLog(data);
                 loggingContext.addLog(data.query.pages);
@@ -26,7 +28,8 @@ const WikipediaSearchPage = () => {
                 // Adding pages keys to array
                 setSearchKeys(Object.keys(results));
 
-                switch(resultQuantity.key){
+                // true for many, false for one
+                switch(searchQuantity){
                     case "many": {
                         // Extracting many items from query and putting them into foundResults
                         Object.keys(results).map(key => {
@@ -72,11 +75,23 @@ const WikipediaSearchPage = () => {
 
     return (
         <div>
-            <h2>Search Wikipedia</h2>
             <div className="col-xs-9 col-md-7" id={"inputFieldsLogin"}>
+                <h2>Search <img src={require('./media/wikipedia.png')} /> Wikipedia</h2>
+                <div className="pretty p-icon p-curve p-jelly">
+                    <input type="radio" name="radio66" onClick={() => setSearchQuantity("one")} />
+                    <div className="state p-warning">
+                        <i className="icon mdi mdi-check"></i>
+                        <label> One</label>
+                    </div>
+                </div>
+                <div className="pretty p-icon p-curve p-jelly">
+                    <input type="radio" name="radio66" onClick={() => setSearchQuantity("many")} defaultChecked="checked"/>
+                    <div className="state p-warning">
+                        <i className="icon mdi mdi-check"></i>
+                        <label> Many</label>
+                    </div>
+                </div>
                 <InputGroup className={"inputGroupLogin"}>
-                    <InputGroupAddon addonType="append"><Button onClick={() => fetchResults({key: "one"})}>OneResult</Button></InputGroupAddon>
-                    <InputGroupAddon addonType="append"><Button onClick={() => fetchResults({key: "many"})}>ManyResults</Button></InputGroupAddon>
                     <Input type="text" value={searchQuery} placeholder="Query..." onChange={(e) => setSearchQuery(e.target.value)}/>
                     <InputGroupAddon addonType="append"><Button onClick={() => fetchResults()}>Search</Button></InputGroupAddon>
                 </InputGroup>
