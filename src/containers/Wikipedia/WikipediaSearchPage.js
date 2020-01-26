0d8bfc5f4a4e246b0337424f8cda4e945a56f22f
Nikolas Graham <Lichtschwert@live.com>
2020-01-25 22:53:52 -0800

    Added a double search to ensure correct top result

--- src/containers/Wikipedia/WikipediaSearchPage.js
+++ src/containers/Wikipedia/WikipediaSearchPage.js
@@ -10,22 +10,48 @@ const WikipediaSearchPage = () => {
 
     const [searchQuery, setSearchQuery] = useState("");
     const [foundResults, setFoundResults] = useState(["Hello", "Testing"]);
+    const [searchKeys, setSearchKeys] = useState([]);
 
-    const fetchResults = () => {
+    const fetchResults = (resultQuantity) => {
         //const endpoint = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=${searchQuery}`;
-        const endpoint = `https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=extracts|pageimages&pithumbsize=400&origin=*&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=${searchQuery}`;
+          const endpoint = `https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=extracts|pageimages&pithumbsize=400&origin=*&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=${searchQuery}`;
         fetch(endpoint)
             .then(response => response.json())
             .then(data => {
+                loggingContext.addLog(data);
                 loggingContext.addLog(data.query.pages);
                 const results = data.query.pages;
                 let tempResults = [];
 
-                // Extracting all items from query and putting them into foundResults
-                Object.keys(results).map(key => {
-                    loggingContext.addLog(results[key]);
-                    tempResults = [...tempResults, {title: results[key].title, extract: results[key].extract}];
-                });
+                // Adding pages keys to array
+                setSearchKeys(Object.keys(results));
+
+                switch(resultQuantity.key){
+                    case "many": {
+                        // Extracting many items from query and putting them into foundResults
+                        Object.keys(results).map(key => {
+                            loggingContext.addLog(results[key]);
+                            if(results[key].title.toLowerCase() === searchQuery.toLowerCase()){
+                                tempResults = [{title: results[key].title, extract: results[key].extract}];
+                            }
+                        });
+                        Object.keys(results).map(key => {
+                            loggingContext.addLog(results[key]);
+                            if(results[key].title.toLowerCase() !== searchQuery.toLowerCase()){
+                                tempResults = [...tempResults,{title: results[key].title, extract: results[key].extract}];
+                            }
+                        });
+                        break;
+                    }
+                    case "one": {
+                        // Extracting one items from query and putting them into foundResults
+                        let keys = Object.keys(results);
+                        tempResults = [{title: results[keys[0]].title, extract: results[keys[0]].extract}]
+                        break;
+                    }
+                    default: {}
+                }
+
 
                 loggingContext.addLog(tempResults);
                 setFoundResults(tempResults);
@@ -49,11 +75,13 @@ const WikipediaSearchPage = () => {
             <h2>Search Wikipedia</h2>
             <div className="col-xs-9 col-md-7" id={"inputFieldsLogin"}>
                 <InputGroup className={"inputGroupLogin"}>
+                    <InputGroupAddon addonType="append"><Button onClick={() => fetchResults({key: "one"})}>OneResult</Button></InputGroupAddon>
+                    <InputGroupAddon addonType="append"><Button onClick={() => fetchResults({key: "many"})}>ManyResults</Button></InputGroupAddon>
                     <Input type="text" value={searchQuery} placeholder="Query..." onChange={(e) => setSearchQuery(e.target.value)}/>
                     <InputGroupAddon addonType="append"><Button onClick={() => fetchResults()}>Search</Button></InputGroupAddon>
                 </InputGroup>
             </div>
-            <div>
+            <div className="col-xs-9 col-md-7" style={{margin: "auto"}}>
                 {
                     foundResults.length > 0 ?
                         foundResults.map((item) => {
