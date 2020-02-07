ae7f1462235d603ebb8ceb480ca7a73878cd0800
Nikolas Graham <Lichtschwert@live.com>
2020-02-06 16:54:36 -0800

    Clearing comment

--- src/hooks/useSpeechRecognition.js
+++ src/hooks/useSpeechRecognition.js
@@ -128,7 +128,6 @@ const useSpeechRecognition = () => {
                 }
                 if ((value.toString().toLocaleLowerCase().includes("mirror mirror search for") && command.toString().toLocaleLowerCase().includes("mirror mirror search for")
                 || (value.toString().toLocaleLowerCase().includes("mirror mirror search wikipedia for") && command.toString().toLocaleLowerCase().includes("mirror mirror search wikipedia for")))) {
-                    // if (value.length === command.length) {
                         if (!commandFound) {
                             if (intent["answer"]) {
                                 speechSynthesisHook.speak(intent["answer"]);
@@ -138,7 +137,6 @@ const useSpeechRecognition = () => {
                             }
                         }
                         commandFound = true;
-                    // }
                 }
             }
         });
