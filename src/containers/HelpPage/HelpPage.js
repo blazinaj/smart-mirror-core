f915286f2993eda83096846269673b0d127c3b12
Anatoli Railean <arailean@eagles.ewu.edu>
2020-02-08 17:13:48 -0800

    Null check

--- src/containers/HelpPage/HelpPage.js
+++ src/containers/HelpPage/HelpPage.js
@@ -13,10 +13,14 @@ const HelpPage = () => {
                 <Col lg={5}>
                     <h3 style={{color: "blue"}}>Page Commands</h3>
                     <h5><label style={{color: "red", fontSize: "28px"}}>Home: </label> Mirror mirror go home</h5>
-                    <h5><label style={{color: "red", fontSize: "28px"}}>Voice Demo: </label> Mirror mirror go to voice demo page</h5>
-                    <h5><label style={{color: "red", fontSize: "28px"}}>Face Demo: </label> Mirror mirror go to face demo page</h5>
-                    <h5><label style={{color: "red", fontSize: "28px"}}>Painting Canvas: </label> Mirror mirror I want to paint</h5>
-                    <h5><label style={{color: "red", fontSize: "28px"}}>Click Game: </label> Mirror mirror I want to play click me game</h5>
+                    <h5><label style={{color: "red", fontSize: "28px"}}>Voice Demo: </label> Mirror mirror go to voice
+                        demo page</h5>
+                    <h5><label style={{color: "red", fontSize: "28px"}}>Face Demo: </label> Mirror mirror go to face
+                        demo page</h5>
+                    <h5><label style={{color: "red", fontSize: "28px"}}>Painting Canvas: </label> Mirror mirror I want
+                        to paint</h5>
+                    <h5><label style={{color: "red", fontSize: "28px"}}>Click Game: </label> Mirror mirror I want to
+                        play click me game</h5>
                     <h5><label style={{color: "red", fontSize: "28px"}}>Help page: </label> Mirror mirror help page</h5>
                     <h5><label style={{color: "red", fontSize: "28px"}}>Sleep: </label> Mirror mirror go to sleep</h5>
                     <h5><label style={{color: "red", fontSize: "28px"}}>Logout: </label> Mirror mirror logout</h5>
@@ -24,20 +28,25 @@ const HelpPage = () => {
                 </Col>
                 <Col>
                     <h3 style={{color: "blue"}}>Misc Commands</h3>
-                    <label style={{color: "red", fontSize: "28px"}}>Show/Hide Speech to Text: </label><h5> Mirror mirror show/hide transcript</h5>
-                    <label style={{color: "red", fontSize: "28px"}}>Create new Account: </label><h5> Mirror mirror register new account</h5>
-                    <label style={{color: "red", fontSize: "28px"}}>Tell Time: </label><h5> Mirror mirror what time is it?</h5>
-                    <label style={{color: "red", fontSize: "28px"}}>Setup Facial Recognition: </label><h5> Mirror mirror ?</h5>
+                    <label style={{color: "red", fontSize: "28px"}}>Show/Hide Speech to Text: </label><h5> Mirror mirror
+                    show/hide transcript</h5>
+                    <label style={{color: "red", fontSize: "28px"}}>Create new Account: </label><h5> Mirror mirror
+                    register new account</h5>
+                    <label style={{color: "red", fontSize: "28px"}}>Tell Time: </label><h5> Mirror mirror what time is
+                    it?</h5>
+                    <label style={{color: "red", fontSize: "28px"}}>Setup Facial Recognition: </label><h5> Mirror mirror
+                    ?</h5>
                 </Col>
                 <Col>
                     <h3 style={{color: "blue"}}>Other</h3>
                     <h5>
                         To setup your Google calendar, do so from the Account Manager on the Home Page.
-                        <hr />
-                        From the Account manager you can also change your name so we know what to call you when you login.
-                        <hr />
+                        <hr/>
+                        From the Account manager you can also change your name so we know what to call you when you
+                        login.
+                        <hr/>
                         Thank you for using our
-                        <br />
+                        <br/>
                         Magic Mirror, by OpTech.
                     </h5>
                 </Col>
@@ -45,11 +54,12 @@ const HelpPage = () => {
                     <h3 style={{color: "blue"}}>All Commands</h3>
                     <h5 style={{color: "blue"}}>(Some commands only work on certain pages)</h5>
                     {
-                        voiceContext.intendArray.map((command) => {
+                        voiceContext && voiceContext.intendArray && voiceContext.intendArray.current && voiceContext.intendArray.current.map((intend) => {
                             return <div>
                                 {
-                                    command.command.map((command) => {
-                                        return <a style={{fontSize: "18px"}}> {JSON.stringify(command)} </a>
+                                    intend && intend.command && intend.command.map((command) => {
+                                        return <a
+                                            style={{fontSize: "18px"}}> {JSON.stringify(command ? command : null)} </a>
                                     })
                                 }
                                 <hr style={{backgroundColor: "red"}}/>
@@ -58,7 +68,7 @@ const HelpPage = () => {
                     }
                 </div>
             </Row>
-            <hr />
+            <hr/>
         </div>
     )
 
