a2822e8b935e77ba3ed00f76994be8b3ed54283e
Nikolas Graham <Lichtschwert@live.com>
2020-02-09 18:25:50 -0800

    Added french joke command

--- src/containers/FrenchDemoPage/FrenchDemoPage.js
+++ src/containers/FrenchDemoPage/FrenchDemoPage.js
@@ -1,26 +1,50 @@
-import React, {useContext, useEffect} from "react";
+import React, {useContext, useEffect, useState} from "react";
 import {VoiceCommandsContext} from "../../context/VoiceCommandsContext";
-import {Row, Col} from "reactstrap"
+import {Row, Col, Button} from "reactstrap"
 
 const RussianDemoPage = () => {
+
+    const [commandsText, setCommandsText] = useState("Commandes");
+    const [getAJokeText, setGetAJokeText] = useState("Obtenez une blague");
+    const [jokeQuestion, setJokeQuestion] = useState("");
+    const [jokeAnswer, setJokeAnswer] = useState("");
 
     const {SpeechRecognitionHook} = useContext(VoiceCommandsContext);
 
     const speakFrench = {
-        command: ["Speak in French"],
+        command: ["mirror mirror speak in French"],
         answer: "Oui, je parle français",
         func: () => {
+            setCommandsText("Commandes");
+            setGetAJokeText("Obtenez une blague");
             SpeechRecognitionHook.changeLanguage('fr-FR');
             SpeechRecognitionHook.setLangVoice('fr-FR');
         }
     };
 
+    const speakingFrench = {
+        command: ["miroir miroir est-ce que tu parles français"],
+        answer: "Nous parlons maintenant, n'est-ce pas?",
+        func: () => {
+        }
+    };
+
     const speakEnglish = {
-        command: ["parlez-vous anglais"],
+        command: ["miroir miroir parlez-vous anglais"],
         answer: "Yes, I speak English",
         func: () => {
+            setCommandsText("Commands");
+            setGetAJokeText("Get a Joke");
             SpeechRecognitionHook.changeLanguage('en-AU');
             SpeechRecognitionHook.setLangVoice('en-US');
+        }
+    };
+
+    const tellMeAJokeInFrench = {
+        command: ["miroir miroir raconte-moi une blague"],
+        answer: "Oui",
+        func: () => {
+            getFrenchJoke();
         }
     };
 
@@ -30,24 +54,69 @@ const RussianDemoPage = () => {
 
         SpeechRecognitionHook.addCommand(speakEnglish);
         SpeechRecognitionHook.addCommand(speakFrench);
+        SpeechRecognitionHook.addCommand(tellMeAJokeInFrench);
+        SpeechRecognitionHook.addCommand(speakingFrench);
 
         return () => {
             SpeechRecognitionHook.removeCommand(speakEnglish);
             SpeechRecognitionHook.removeCommand(speakFrench);
+            SpeechRecognitionHook.removeCommand(tellMeAJokeInFrench);
+            SpeechRecognitionHook.removeCommand(speakingFrench);
         }
 
     }, []);
 
+    // Currently not in use, saving for later
+    const getJoke = () => {
+        fetch('https://sv443.net/jokeapi/v2/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist')
+            .then((response) => {
+                return response.json();
+            })
+            .then((myJson) => {
+
+                console.log(myJson);
+            });
+    };
+
+    const getFrenchJoke = () => {
+        const proxyurl = "https://cors-anywhere.herokuapp.com/";
+        const url = 'https://blague.xyz/api/joke/random';
+
+        fetch(proxyurl + url, {
+            headers: {
+                'Authorization': 'oG6tKHDZMT_99C-vWDti.S6F_euYPnrd4nwtSWOza00mkZCoFZhHXp8g_ijMooTv'
+            }
+        })
+            .then((resp) => resp.json())
+            .then((data) => {
+                console.log(data);
+                setJokeQuestion(data.joke.question);
+                setJokeAnswer(data.joke.answer);
+            })
+            .catch(err => console.log("Error getting french joke: " + err));
+    };
+
     return (
         <>
             <Row>
-                <Col sm={12}>
-                    <h2>parles tu français?</h2>
+                <Col>
+                    <Button color="primary" onClick={() => getFrenchJoke()}>{getAJokeText}</Button>
                 </Col>
-                <Col sm={12}>
-                    <h2>Commands</h2>
-                    <h5>Speak in French</h5>
-                    <h5>parlez-vous anglais</h5>
+                <Col>
+                    <h2>parles tu français?</h2>
+                    <br/>
+                    <br/>
+                    <h4>Blagues</h4>
+                    <hr/>
+                    <h5 style={{color: "blue"}}>{jokeQuestion}</h5>
+                    <h5 style={{color: "#8E1600"}}>{jokeAnswer}</h5>
+                </Col>
+                <Col>
+                    <h2>{commandsText}</h2>
+                    <h5>Mirror Mirror Speak in French</h5>
+                    <h5>Miroir miroir parlez-vous anglais?</h5>
+                    <h5>Miroir miroir raconte moi une blague</h5>
+                    <h5>miroir miroir est-ce que tu parles français</h5>
                 </Col>
             </Row>
         </>
