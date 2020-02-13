2c66569e96b64cd3ceec2f87580dc907de605f13
Art Elikh <aelikh@gmail.com>
2020-02-12 16:28:18 -0800

    Adding speech to inspiration

--- src/components/Inspiration/Inspiration.js
+++ src/components/Inspiration/Inspiration.js
@@ -4,14 +4,25 @@ import {Input, Label, Button, Row, Col} from 'reactstrap';
 import {VoiceCommandsContext} from "../../context/VoiceCommandsContext";
 import axios from 'axios';
 import AnalogClock from "analog-clock-react";
-// import Math from Math;
+import useSpeechSynthesis from "../../hooks/useSpeechSynthesis";
+
 
 
 
 
 const Inspiration = (props)=>{
-    //const {SpeechRecognitionHook} = useContext(VoiceCommandsContext);
-    const [quote,setQuote] =  useState(null); 
+
+    const {SpeechRecognitionHook} = useContext(VoiceCommandsContext);
+    const [quote,setQuote] =  useState(null);
+    const [readText, setReadText] = useState( null);
+    const speechSynthesisHook = useSpeechSynthesis();
+
+    //NEEDS WORK
+    const readCommand = {
+        command: ["mirror mirror read my quote"],
+        answer: "ok reading again!",
+        func: () => readMyQuote(JSON.stringify(readText))
+    };
     
 
 // clock widget colors
@@ -28,21 +39,45 @@ const Inspiration = (props)=>{
         }
     };
 
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * Math.floor(max));
+    }
+//NEEDS WORK
+    function readMyQuote(readText){
+        console.log(readText)
+        speechSynthesisHook.speak(readText)
+    }
 
-    useEffect(() => { 
+
+    const getRandomQuote=() => {
         axios.get("https://type.fit/api/quotes")
-    
+
         // axios.get("http://quotes.rest/qod.json?category=funny") // only 10 per hr
         .then(responce => {
-            const myJASON = responce.data; 
-            //console.log(myJASON)
-            // console.log(Math.round(Math.random() * 1643)
-            console.log(myJASON[1642].text +" "+ myJASON[1642].author)
-                 
-            setQuote(myJASON[1602].text +' - '+ myJASON[1602].author) ;
+            const myJSON = responce.data;
+            const qindex = getRandomInt(myJSON.length);
+            const sayThis = JSON.stringify(myJSON[(qindex)].text + " said " +myJSON[(qindex)].author )
+            setReadText(JSON.stringify(myJSON[(qindex)].text + " said " +myJSON[(qindex)].author ));
+            //console.log(myJSON)
+            //console.log(sayThis)
+            speechSynthesisHook.speak(sayThis)// TEMP for voice read
+            setQuote(myJSON[(qindex)].text +' - '+ myJSON[(qindex)].author) ;
         });
-    },[]);
+    }
+
+
+// i need this for categorized fetch of quotes
+//"categories": {
+//             "inspire": "Inspiring Quote of the day",
+//             "management": "Management Quote of the day",
+//             "sports": "Sports Quote of the day",
+//             "life": "Quote of the day about life",
+//             "funny": "Funny Quote of the day",
+//             "love": "Quote of the day about Love",
+//             "art": "Art quote of the day ",
+//             "students": "Quote of the day for students"
 //functioning version
+
 // useEffect(() => { 
 //     //axios.get("https://type.fit/api/quotes")
 
@@ -55,6 +90,19 @@ const Inspiration = (props)=>{
 //         setQuote(myJASON.contents.quotes[0].quote +'\n'+ myJASON.contents.quotes[0].author) ;
 //     });
 // },[]);
+
+    useEffect(() => {
+        getRandomQuote()
+        //NEED WORK
+        // readMyQuote(JSON.stringify(readText))
+    },[]);
+
+    useEffect(() => {
+        SpeechRecognitionHook.addCommand(readCommand);
+        return () => {
+            SpeechRecognitionHook.removeCommand(readCommand);
+        }
+    }, []);
 
     return (
         <div style={{height: "100vh", background: "black", padding: "5vw"}}>
