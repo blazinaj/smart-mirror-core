175dbe937762c22127246eba50a47e788c9d5b47
Unknown <jhowes@eagles.ewu.edu>
2020-02-20 11:45:15 -0800

    Initial code for IoT device created

--- src/components/IoTSandbox/IoTSandbox.js
+++ src/components/IoTSandbox/IoTSandbox.js
@@ -9,26 +9,37 @@ import awsIot from 'aws-iot-device-sdk';
 // to connect with a client identifier which is already in use, the existing
 // connection will be terminated.
 //
-const device =  awsIot.device({
-    keyPath: <YourPrivateKeyPath>,
-        certPath: <YourCertificatePath>,
-            caPath: <YourRootCACertificatePath>,
-                clientId: <YourUniqueClientIdentifier>,
-                    host: <YourCustomEndpoint>
-                        });
+const device = ()=> {
+    awsIot.device({
+        keyPath: "../../../../public/cert/d5efb49d34-private.pem.key",
+        certPath: "../../../../public/cert/d5efb49d34-public.pem.key",
+        caPath: "../../../../public/cert/AmazonRootCA1.pem.txt",
+        clientId: "arn:aws:iot:us-west-2:615420348778:thing/Smart_Mirror_IoT",
+        host: "Smart_Mirror_IoT"
+    });
 
-                        //
-                        // Device is an instance returned by mqtt.Client(), see mqtt.js for full
-                        // documentation.
-                        //
-                        device
-                        .on('connect', function() {
-                            console.log('connect');
-                        device.subscribe('topic_1');
-                        device.publish('topic_2', JSON.stringify({ test_data: 1}));
-                        });
+    //
+    // Device is an instance returned by mqtt.Client(), see mqtt.js for full
+    // documentation.
+    //
+    device
+        .on('connect', function () {
+            console.log('connect');
+            device.subscribe('topic_1');
+            device.publish('topic_2', JSON.stringify({test_data: 1}));
+        });
 
-                        device
-                        .on('message', function(topic, payload) {
-                        console.log('message', topic, payload.toString());
-                    });;
+    device
+        .on('message', function (topic, payload) {
+            console.log('message', topic, payload.toString());
+        });
+
+    return(
+        <div>
+            <device/>
+        </div>
+    );
+
+};
+
+export default device;
